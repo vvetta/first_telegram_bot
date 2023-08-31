eada5d362c96805af0786fb30d2577baab5131c8
vvetta <drpsih2004@gmail.com>
2023-08-31 15:19:56 +0300

    on_startup, sticker handler

--- bot_files/main.py
+++ bot_files/main.py
@@ -6,11 +6,16 @@ from secret import API_TOKEN
 HELP_COMMAND = """
 /help - список команд
 /start - запуск бота
+/sticker - класный стикер
 """
 
 
 bot = aiogram.Bot(API_TOKEN)
 dp = aiogram.Dispatcher(bot=bot)
+
+
+async def on_startup(_):
+    print('Бот был успешно запущен!')
 
 
 # @dp.message_handler()
@@ -20,13 +25,23 @@ dp = aiogram.Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['help'])
 async def help_command(message: aiogram.types.Message):
-    await message.reply(text=HELP_COMMAND)
+    await message.reply(text=HELP_COMMAND) # reply - ответ на сообщение пользователя
     
     
 @dp.message_handler(commands=['start'])
 async def start_command(message: aiogram.types.Message):
-    await message.answer(text='Добро пожаловать!')
+    await message.answer(text='Добро пожаловать!🤡\n'
+                                'Список команд бота: \n')
+    await message.answer(text=HELP_COMMAND)
+    # await message.delete() # Удаляет сообщение пользователя
+    # await message.answer('<strong>Тут будет сообщение</strong>', parse_mode='HTML')
+
+
+@dp.message_handler(commands=['sticker'])
+async def get_sticker(message: aiogram.types.Message):
+    await bot.send_sticker(message.from_user.id, 
+    sticker='CAACAgUAAxkBAAEKLgFk8IMUjpIbfQLp6r4nEgk0AvNQBgACGQMAAo0JiVWW-4DaQ3diujAE')
 
 
 if __name__ == '__main__':
-    aiogram.executor.start_polling(dp)
+    aiogram.executor.start_polling(dp, on_startup=on_startup)
