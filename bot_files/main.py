1d4c5e1d2ec57354b32a78d814f3f42957bdb398
vvetta <drpsih2004@gmail.com>
2023-08-31 14:40:58 +0300

    add two handlers (help, start)

--- bot_files/main.py
+++ bot_files/main.py
@@ -1,16 +1,31 @@
 import aiogram
 
 # from secret.py
-from bot_files.secret import API_TOKEN
+from secret import API_TOKEN
+
+HELP_COMMAND = """
+/help - список команд
+/start - запуск бота
+"""
 
 
 bot = aiogram.Bot(API_TOKEN)
 dp = aiogram.Dispatcher(bot=bot)
 
 
-@dp.message_handler()
-async def echo(message: aiogram.types.Message):
-    await message.answer(text=message.text) 
+# @dp.message_handler()
+# async def echo(message: aiogram.types.Message):
+#     await message.answer(text=message.text) 
+
+
+@dp.message_handler(commands=['help'])
+async def help_command(message: aiogram.types.Message):
+    await message.reply(text=HELP_COMMAND)
+    
+    
+@dp.message_handler(commands=['start'])
+async def start_command(message: aiogram.types.Message):
+    await message.answer(text='Добро пожаловать!')
 
 
 if __name__ == '__main__':
